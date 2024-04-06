1a736300999950077b3e5124fb5ea1421e6342ef
Marko Ticinovic <markoticinovic25@gmail.com>
2024-04-06 22:53:38 +0200

    Form fixed

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -3,6 +3,7 @@ import classes from "./page.module.css";
 import Image from "next/image";
 import { notFound } from "next/navigation";
 
+
 export default function MealsDetailsPage({ params }) {
   const meal = getMeal(params.slug);
 
